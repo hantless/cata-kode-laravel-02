41e4d1b0c821b9490077bbbe660f68d1e35332f4
Frédéric Arijs <hantless@gmail.com>
2022-06-19 22:45:29 +0200

    Base route and Controller for the appointments

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    public function show()
+    {
+        return view('appointment.show');
+    }
+}
